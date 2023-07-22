688de00afa90bdb063a6660aa6c8671adf4d0af9
Itb4ng <bsh801099@gmail.com>
2023-07-23 05:11:36 +0900

    Update

---
+++ data/ipads.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    thumbnail: './images/compare_ipad_pro.png',
+    colors: ['#68696D', '#E2E3E5'],
+    name: 'iPad Pro',
+    tagline: '최첨단 기술이 구현하는br />궁극의 iPad 경험.',
+    price: 1249000,
+    url: '/ipad-pro/'
+  },
+  {
+    thumbnail: './images/compare_ipad_air.png',
+    colors: ['#68696D', '#E2E3E5', '#E8D2CE', '#B9B8D1', '#88AEBF'],
+    name: 'iPad Air',
+    tagline: '얇고 가벼운 디자인.<br />결코 가볍지 않은 성능.',
+    price: 929000,
+    url: '/ipad-air/'
+  },
+  {
+    thumbnail: './images/compare_ipad_10_2.png',
+    colors: ['#68696D', '#E2E3E5'],
+    name: 'iPad',
+    tagline: '다양한 일상 작업에 맞는<br />완전히 새롭고, 컬러풀한 iPad.',
+    price: 679000,
+    url: '/ipad-10.9/'
+  },
+  {
+    thumbnail: './images/compare_ipad_mini.png',
+    colors: ['#68696D', '#E2E3E5', '#E8D2CE', '#B9B8D1'],
+    name: 'iPad mini',
+    tagline: '한 손에 쏙 들어오는<br />손색없는 iPad 경험.',
+    price: 649000,
+    url: '/ipad-mini/'
+  }
+]
