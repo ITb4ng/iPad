146d059ce0d8e3fb841bf8b21552cd6bea8d7d0c
ITb4ng <105259684+ITb4ng@users.noreply.github.com>
2026-05-09 22:17:49 +0900

    refactor(asset): 이미지 폴더 구조 정리와 미사용 자산 제거

--- data/ipads.js
+++ data/ipads.js
@@ -1,6 +1,6 @@
 export default [
   {
-    thumbnail: './images/compare_ipad_pro.png',
+    thumbnail: './images/compare/compare-ipad-pro.png',
     colors: ['#68696D', '#E2E3E5'],
     name: 'iPad Pro',
     tagline: '최첨단 기술이 구현하는 궁극의 iPad 경험.',
@@ -8,7 +8,7 @@ export default [
     url: '/buy/ipad-pro'
   },
   {
-    thumbnail: './images/compare_ipad_air.png',
+    thumbnail: './images/compare/compare-ipad-air.png',
     colors: ['#68696D', '#E2E3E5', '#E8D2CE', '#B9B8D1', '#88AEBF'],
     name: 'iPad Air',
     tagline: '얇고 가벼운 디자인. 결코 가볍지 않은 성능.',
@@ -16,7 +16,7 @@ export default [
     url: '/buy/ipad-air'
   },
   {
-    thumbnail: './images/compare_ipad_10_2.png',
+    thumbnail: './images/compare/compare-ipad-10-2.png',
     colors: ['#68696D', '#E2E3E5'],
     name: 'iPad',
     tagline: '매일매일 여러 일을 척척. 널찍한 화면. 화사한 색상',
@@ -24,7 +24,7 @@ export default [
     url: '/buy/ipad-11'
   },
   {
-    thumbnail: './images/compare_ipad_mini.png',
+    thumbnail: './images/compare/compare-ipad-mini.png',
     colors: ['#68696D', '#E2E3E5', '#E8D2CE', '#B9B8D1'],
     name: 'iPad mini',
     tagline: 'iPad다운 경험은 그대로. 휴대성은 최대로.',
